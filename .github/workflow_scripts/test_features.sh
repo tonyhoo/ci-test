e31b6afb3ac439898a8964881e196b14fb71600f
tonyhoo <tonyhoo0410@gmail.com>
2022-11-28 14:05:09 -0800

    Added workflow scripts

---
+++ .github/workflow_scripts/test_features.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+ADDITIONAL_TEST_ARGS=$1
+
+source $(dirname "$0")/env_setup.sh
+
+setup_build_env
+install_common
+install_features
+
+cd features/
+if [ -n "$ADDITIONAL_TEST_ARGS" ]
+then
+    python3 -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+else
+    python3 -m pytest --junitxml=results.xml --runslow tests
+fi
---
+++ .github/workflow_scripts/test_timeseries.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+ADDITIONAL_TEST_ARGS=$1
+
+source $(dirname "$0")/env_setup.sh
+
+setup_build_env
+setup_mxnet_gpu
+export CUDA_VISIBLE_DEVICES=0
+install_core_all_tests
+install_features
+install_tabular_all
+install_timeseries
+
+cd timeseries/
+if [ -n "$ADDITIONAL_TEST_ARGS" ]
+then
+    python3 -m pytest --junitxml=results.xml --runslow "$ADDITIONAL_TEST_ARGS" tests
+else
+    python3 -m pytest --junitxml=results.xml --runslow tests
+fi
